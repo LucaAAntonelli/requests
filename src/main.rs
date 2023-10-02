54605d94e367792c42e79f136636f38e84568a1e
Luca <lucaan@ethz.ch>
2023-10-02 18:55:58 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use chrono::{DateTime, Utc};
 extern crate reqwest;
 use std::io;
+use serde::Deserialize;
+use serde_json::Value;
 
+#[derive(Debug, Deserialize)]
 struct Book {
     title: String,
-    author: String,
-    release_date: DateTime<Utc>,
+    author: Vec<String>,
     pages: u16,
 }
 #[tokio::main]
@@ -15,7 +16,10 @@ async fn main() {
     io::stdin().read_line(&mut query).expect("Failed to read line");
     println!("Calling search function with query={query}");
     let value = search(query).await.expect("Query returned an error");
-    println!("The query returned the following result:\n{value}");
+//  println!("The query returned the following result:\n{value}");
+    println!("Trying to call json_to_book on result");
+    let book = json_to_book(value);
+    //println!("Book: {book:?}");
 }
 
 async fn search(query: String) -> Result<String, reqwest::Error> {
@@ -25,4 +29,22 @@ async fn search(query: String) -> Result<String, reqwest::Error> {
         .await?;
 
     Ok(body)
+}
+
+fn json_to_book(json: String) -> Book {
+    let book = Book {title: String::from("asdf"), author: vec![String::from("asdf")], pages: 0};
+    let collection: Value = serde_json::from_str(&json).expect("Failed to parse JSON");
+    println!("{collection}");
+
+    // for val in collection.as_object().unwrap() {
+    //     let (key, v) = val;
+    //     println!("key={key}");
+    //     println!("v={v}");
+    //     if key == "VolumeInfo" {
+    //         println!("values={v}");
+    //     } else {
+    //         println!("key not found");
+    //     }
+    // }
+    book
 }
