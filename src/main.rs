55cf3129716f55cdd515502a2fda874e47ab81cb
Luca <lucaan@ethz.ch>
2023-10-02 22:37:37 +0200

    json_to_book prints extracted title and authors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate reqwest;
-use std::io;
 use serde::Deserialize;
 use serde_json::Value;
+use std::io;
 
 #[derive(Debug, Deserialize)]
 struct Book {
@@ -13,10 +13,12 @@ struct Book {
 async fn main() {
     let mut query = String::new();
     println!("Enter book to search");
-    io::stdin().read_line(&mut query).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut query)
+        .expect("Failed to read line");
     println!("Calling search function with query={query}");
     let value = search(query).await.expect("Query returned an error");
-//  println!("The query returned the following result:\n{value}");
+    //  println!("The query returned the following result:\n{value}");
     println!("Trying to call json_to_book on result");
     let book = json_to_book(value);
     //println!("Book: {book:?}");
@@ -32,19 +34,18 @@ async fn search(query: String) -> Result<String, reqwest::Error> {
 }
 
 fn json_to_book(json: String) -> Book {
-    let book = Book {title: String::from("asdf"), author: vec![String::from("asdf")], pages: 0};
+    let book = Book {
+        title: String::from("asdf"),
+        author: vec![String::from("asdf")],
+        pages: 0,
+    };
     let collection: Value = serde_json::from_str(&json).expect("Failed to parse JSON");
-    println!("{collection}");
+    let items = &collection["items"].as_array();
+    for item in items.unwrap() {
+        let title = &item["volumeInfo"]["title"];
+        let authors = &item["volumeInfo"]["authors"];
+        println!("{title}, {authors}");
+    }
 
-    // for val in collection.as_object().unwrap() {
-    //     let (key, v) = val;
-    //     println!("key={key}");
-    //     println!("v={v}");
-    //     if key == "VolumeInfo" {
-    //         println!("values={v}");
-    //     } else {
-    //         println!("key not found");
-    //     }
-    // }
     book
 }
